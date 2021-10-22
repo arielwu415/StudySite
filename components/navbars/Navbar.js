5f61cd4640cdaa4e25319f2d03579e6522e49ae7
arielwu415 <ariel070415@gmail.com>
2021-10-21 17:04:20 -0700

    separate styles from index.js

---
+++ components/navbars/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function Navbar() {
+    retrun (
+        <header>
+            <nav className = "nav_elements">
+                <div className = "left_elements">
+
+                </div>
+            </nav>
+        </header>
+    )
+}
