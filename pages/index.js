0eee2f1d813cc49684ed143d1bef9208552b4d33
arielwu415 <ariel070415@gmail.com>
2021-10-28 22:25:39 -0700

    added background image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/style.module.css'
 import Layout from '../components/layouts/Layout'
 import Navbar from '../components/navbars/Navbar'
@@ -12,50 +13,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar/>
+      <Navbar />
       <main id={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a id={styles.a} href="https://nextjs.org">JavaScript Capstone Project!</a>
-        </h1>
+          <div className={styles.main_container}>
+            Working area
+            <div className={styles.timer}></div>
+          </div>
+          <div className={styles.background_container}>
+            <Image
+              className={styles.background}
+              src="/bg_sunset.jpg"
+              alt="Sunset Mode"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
 
-        <p id={styles.p} className={styles.description}>
-          Get started by editing <code id={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a id={styles.a} href="https://nextjs.org/docs" className={styles.card}>
-            <h3 id={styles.h3}>Documentation &rarr;</h3>
-            <p id={styles.p}>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a id={styles.a} href="https://nextjs.org/learn" className={styles.card}>
-            <h3 id={styles.h3}>Learn &rarr;</h3>
-            <p id={styles.p}>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            id={styles.a}
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3 id={styles.h3}>Examples &rarr;</h3>
-            <p id={styles.p}>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            id={styles.a}
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3 id={styles.h3}>Deploy &rarr;</h3>
-            <p id={styles.p}>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer id={styles.footer}>
+      {/*<footer id={styles.footer}>
         <a
           id={styles.a}
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -65,7 +42,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
-      </footer>
+      </footer>*/}
       <style jsx global>{`
         html,
         body {
