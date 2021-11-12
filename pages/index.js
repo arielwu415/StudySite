d7741f3f0b2ec86d0a54c18c48846a17fc1397a6
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2021-11-11 22:59:35 -0800

    Index modification

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ import styles from '../styles/style.module.scss'
 import Navbar from '../components/navbars/Navbar'
 import GlobalStlye from '../components/pageStyles/GlobalStyle'
 import PageStyle from '../components/pageStyles/PageStyle'
-import ModalBox from '../components/cards/ModalBox'
 
 import { useState } from 'react'
 
 export default function Home() {
   const [night, setNight] = useState(false)
   const [start, setStart] = useState(false)
-
-  const [showModal, setShowModal] = useState(false)
 
   return (
     <div className={styles.container}>
@@ -26,8 +23,7 @@ export default function Home() {
       </Head>
       <Intro />
       <Navbar onNightToggle={setNight} />
-      <Main start={start} night={night} setStart={setStart} setShowModal={setShowModal} />
-      <ModalBox showModal={showModal} setShowModal={setShowModal} />
+      <Main start={start} night={night} setStart={setStart}/>
     </div>
   )
 }
